d1c8f1363ca0b377d0e63a02b6142a2396e3b94a
Abdulsalam <adektunji@gma>
2022-10-16 00:13:57 -0700

    Task 3-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main: is a function that contains all c codes
+ * Return: This always return 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}

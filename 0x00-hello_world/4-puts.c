9c7b690ee0c0b32c302765f62c361629abeaa0a2
Abdulsalam <adektunji@gma>
2022-10-16 00:32:54 -0700

    4-put edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-//A C programm that prints a text using puts command
 #include <stdio.h>
 /**
  * main: is a function that contains all c codes
@@ -6,8 +5,6 @@
  */
 int main(void)
 {
-	//writing a text to the std output
 	puts("\"Programming is like building a multilingual puzzle\n");
-	//return 0
 	return (0);
 }

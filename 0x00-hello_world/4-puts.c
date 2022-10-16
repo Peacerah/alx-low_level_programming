731b9e15465ac176c9a1132119d751c022012d34
Abdulsalam <adektunji@gma>
2022-10-16 00:27:29 -0700

    4-puts file edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+//A C programm that prints a text using puts command
 #include <stdio.h>
 /**
  * main: is a function that contains all c codes
@@ -5,6 +6,8 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	//writing a text to the std output
+	puts("\"Programming is like building a multilingual puzzle\n");
+	//return 0
 	return (0);
 }

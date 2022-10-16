f8414ca4d9935d2aa1b9113e79cd249a18727c34
Abdulsalam <adektunji@gma>
2022-10-16 00:55:12 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * A programm that prints to the standard output
+ * main: The main function where coding is initiated
+ * return: always return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

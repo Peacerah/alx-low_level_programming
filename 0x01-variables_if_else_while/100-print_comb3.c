6fc06a0c840b8ae40d49efc851b757d421363ce1
Abdulsalam <adektunji@gma>
2022-10-19 09:15:57 -0700

    Task 100 done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,6 @@
 
 int main(void)
 {
-
 	int n1 = 48;
 
 	int n2;
@@ -27,35 +26,20 @@ int main(void)
 
 	{
 		n2 = n1 + 1;
-
 		while (n2 <= 57)
-
 		{
-
 			putchar(n1);
-
 			putchar(n2);
-
 			if (n1 != 56 || n2 != 57)
-
 			{
-
 				putchar(com);
-
 				putchar(32);
-
 			}
-
 			n2 += 1;
-
 		}
-
 		n1 += 1;
-
 	}
-
 	putchar('\n');
-
 	return (0);
 
 }

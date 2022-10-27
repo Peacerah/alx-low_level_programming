e473db2ce2402e9bacaa58a9959b0b467185987b
Abdulsalam <adektunji@gma>
2022-10-27 09:47:28 -0700

    Task 3 done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) - main entry to determine if 
+ * an alphabet is small letter
+ *
+ * Return: Always return 1 if true or 0 if otherwise 
+ *
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);

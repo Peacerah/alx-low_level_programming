09439f8ef6c09ea3da29e97af9918953d8f38c40
Abdulsalam <adektunji@gma>
2022-11-17 05:29:00 -0800

    Task 0 to 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add - funtion to add
+ * @a: input 1
+ * @b: input 2
+ * Return: sum a and b
+ */
+int op_add(int a, int b)
+	return (a+b);
+
+/**
+ * op_sub - funtion to subtract
+ * @a: input 1
+ * @b: input 2
+ * Return: sub a and b
+ */
+int op_sub(int a, int b)
+	return ("%d", (a-b));
+
+/**
+ * op_mul - funtion to multiply
+ * @a: input 1
+ * @b: input 2
+ * Return: mul a and b
+ */
+int op_mul(int a, int b)
+	return ("%d", (a * b));
+
+/**
+ * op_div - funtion to div
+ * @a: input 1
+ * @b: input 2
+ * Return: div a and b
+ */
+int op_div(int a, int b)
+	return ("%d", (a/b);
+
+
+/**
+ * op_mod - funtion to modulus
+ * @a: input 1
+ * @b: input 2
+ * Return: mod a and b
+ */
+int op_mod(int a, int b)
+	return ("%d", (a % b));

34c3b3b8759e2e3c2a97aa03f88389cdc203c20a
Abdulsalam <adektunji@gma>
2022-11-18 20:22:31 -0800

    Task 0 - 4

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,6 @@
 #ifndef VARFUNC_H
 #define VARFUNC_H
 #include <stdarg.h>
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -18,4 +17,5 @@ typedef struct type
 	ar *op;
 	void (*f)(va_list, char *);
 } type_t;
+
 #endif

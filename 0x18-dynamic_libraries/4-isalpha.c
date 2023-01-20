3124ce179ef44ba40b11d9083f3ba7dc29c79f45
Abdulsalam <adektunji@gma>
2023-01-20 10:00:23 -0800

    task 0

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for alphabetic character
+ * @c: input argumen to check
+ *
+ * Return: Always int.
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
+
+
